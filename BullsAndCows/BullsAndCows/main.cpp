bf893f4f1e626e589b74216644845834b0662d32
Brent Caulfield <brentcaulfield@gmail.com>
2018-11-10 14:44:51 -0500

    Lecture 22: Using cin to capture input and a string variable to store that input so that we can ask the player for a guess and then repeat it back to them.

--- BullsAndCows/BullsAndCows/main.cpp
+++ BullsAndCows/BullsAndCows/main.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	constexpr int WORD_LENGTH = 5;
-	
+	// introduce the game
+	constexpr int WORD_LENGTH = 5;	
 	cout << "Welcome to Bulls and Cows!" << endl;
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thining of?" << endl;
+
+	// get guess from player
+	string Guess = "";
+	cout << "Please enter a guess: ";
+	cin >> Guess;
+	cout << endl;
+	
+	// repeat the guess back to player
+	cout << "You guessed: " << Guess;
+	cout << endl;
+
+	// get guess from player
+	cout << "Please enter a guess: ";
+	cin >> Guess;
+	cout << endl;
+
+	// repeat the guess back to player
+	cout << "You guessed: " << Guess;
+	cout << endl;
 
 	return 0;
 }
